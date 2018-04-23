20bccdb8270538b1fcbd9fc5cf92c28c343ea135
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomGridResourceText/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Collections.ObjectModel;
+using DevExpress.Xpf.Grid;
+
+namespace CustomGridResourceText {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            GridControlResXLocalizer.Active = new CustomGridLocalizer();
+            
+            InitializeComponent();
+
+            ObservableCollection<Task> data = new ObservableCollection<Task>();
+            gridControl1.ItemsSource = data;
+        }
+    }
+
+    public class Task {
+        private DateTime _DueDate = DateTime.Now;
+        public DateTime DueDate {
+            get {
+                return _DueDate;
+            }
+            set {
+                _DueDate = value;
+            }
+        }
+        public string Subject { get; set; }
+    }
+}
---
+++ VB/CustomGridResourceText/MainWindow.xaml.vb
@@ -0,0 +1,43 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows
+Imports System.Collections.ObjectModel
+Imports DevExpress.Xpf.Grid
+
+Namespace CustomGridResourceText
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			GridControlResXLocalizer.Active = New CustomGridLocalizer()
+
+			InitializeComponent()
+
+			Dim data As New ObservableCollection(Of Task)()
+			gridControl1.ItemsSource = data
+		End Sub
+	End Class
+
+	Public Class Task
+		Private _DueDate As DateTime = DateTime.Now
+		Public Property DueDate() As DateTime
+			Get
+				Return _DueDate
+			End Get
+			Set(ByVal value As DateTime)
+				_DueDate = value
+			End Set
+		End Property
+		Private privateSubject As String
+		Public Property Subject() As String
+			Get
+				Return privateSubject
+			End Get
+			Set(ByVal value As String)
+				privateSubject = value
+			End Set
+		End Property
+	End Class
+End Namespace
