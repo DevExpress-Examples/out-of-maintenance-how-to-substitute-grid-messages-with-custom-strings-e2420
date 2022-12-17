60cc885f02759bead56c27ecc75573effe24e49a
DevExpressExampleBot <fakeEmail>
2022-12-17 13:08:07 +0400

    Source auto update [skip ci]

--- VB/CustomGridResourceText/CustomGridLocalizer.vb
+++ VB/CustomGridResourceText/CustomGridLocalizer.vb
@@ -1,20 +1,14 @@
-Imports Microsoft.VisualBasic
-Imports System
 Imports DevExpress.Xpf.Grid
 
 Namespace CustomGridResourceText
-	Public Class CustomGridLocalizer
-		Inherits GridControlResXLocalizer
-		Public Overrides Function GetLocalizedString(ByVal id As GridControlStringId) As String
-			If id = GridControlStringId.GridNewRowText Then
-				Return "Click here to add a new task"
-			End If
 
-			If id = GridControlStringId.GridGroupPanelText Then
-				Return "Here goes custom text for the group panel"
-			End If
+    Public Class CustomGridLocalizer
+        Inherits GridControlResXLocalizer
 
-			Return MyBase.GetLocalizedString(id)
-		End Function
-	End Class
+        Public Overrides Function GetLocalizedString(ByVal id As GridControlStringId) As String
+            If id = GridControlStringId.GridNewRowText Then Return "Click here to add a new task"
+            If id = GridControlStringId.GridGroupPanelText Then Return "Here goes custom text for the group panel"
+            Return MyBase.GetLocalizedString(id)
+        End Function
+    End Class
 End Namespace
--- VB/CustomGridResourceText/MainWindow.xaml.vb
+++ VB/CustomGridResourceText/MainWindow.xaml.vb
@@ -1,43 +1,37 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Imports System.Windows
 Imports System.Collections.ObjectModel
 Imports DevExpress.Xpf.Grid
 
 Namespace CustomGridResourceText
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			GridControlResXLocalizer.Active = New CustomGridLocalizer()
 
-			InitializeComponent()
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-			Dim data As New ObservableCollection(Of Task)()
-			gridControl1.ItemsSource = data
-		End Sub
-	End Class
+        Public Sub New()
+            GridControlResXLocalizer.Active = New CustomGridLocalizer()
+            Me.InitializeComponent()
+            Dim data As ObservableCollection(Of Task) = New ObservableCollection(Of Task)()
+            Me.gridControl1.ItemsSource = data
+        End Sub
+    End Class
 
-	Public Class Task
-		Private _DueDate As DateTime = DateTime.Now
-		Public Property DueDate() As DateTime
-			Get
-				Return _DueDate
-			End Get
-			Set(ByVal value As DateTime)
-				_DueDate = value
-			End Set
-		End Property
-		Private privateSubject As String
-		Public Property Subject() As String
-			Get
-				Return privateSubject
-			End Get
-			Set(ByVal value As String)
-				privateSubject = value
-			End Set
-		End Property
-	End Class
+    Public Class Task
+
+        Private _DueDate As Date = Date.Now
+
+        Public Property DueDate As Date
+            Get
+                Return _DueDate
+            End Get
+
+            Set(ByVal value As Date)
+                _DueDate = value
+            End Set
+        End Property
+
+        Public Property Subject As String
+    End Class
 End Namespace
